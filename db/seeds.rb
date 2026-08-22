1d1d896d6c04c39a9678ba616e010aa5e6eaa133
Hurnan Bersabe <hurnanalbersabe@gmail.com>
2026-08-22 19:56:38 +0800

    Added teacher and laboratory, also added a pre-data in app/db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,108 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Laboratory.destroy_all
+Teacher.destroy_all
+Student.destroy_all
+Department.destroy_all
+
+computer_Eng = Department.create!(
+  name: "Department of CpE",
+  location: "Dolan Building, 4th Floor"
+)
+
+elec_Eng = Department.create!(
+  name: "Department of EcE",
+  location: "Dolan Building, 4th Floor"
+)
+
+#CpE Students
+
+Student.create!(
+  name: "Hurnan Bersabe",
+  year_level: 3,
+  program: "BS CpE",
+  department: computer_Eng
+)
+
+Student.create!(
+  name: "Kenneth Sarmiento",
+  year_level: 3,
+  program: "BS CpE",
+  department: computer_Eng
+)
+
+Student.create!(
+  name: "JB Rafael",
+  year_level: 3,
+  program: "BS CpE",
+  department: computer_Eng
+)
+
+# EcE Students
+
+Student.create!(
+  name: "John Benedict",
+  year_level: 3,
+  program: "BS EcE",
+  department: elec_Eng
+)
+
+Student.create!(
+  name: "Juan Sabino",
+  year_level: 3,
+  program: "BS EcE",
+  department: elec_Eng
+)
+
+Student.create!(
+  name: "Daniel Ramos",
+  year_level: 3,
+  program: "BS EcE",
+  department: elec_Eng
+)
+
+# Teachers
+
+Teacher.create!(
+  name: "ma'am Karla Sobrevilla",
+  email: "ma'amKarla@gmail.com",
+  specialization: "Master in CpE",
+  department: computer_Eng
+)
+
+Teacher.create!(
+  name: "Sir, Anacleto Casimiro",
+  email: "sirNaki@gmail.com",
+  specialization: "Master in EcE",
+  department: elec_Eng
+)
+
+# Comp Laboratories
+Laboratory.create!(
+  name: "Cisco Laboratory",
+  location: "Dolan Building, 4th Floor",
+  department: computer_Eng
+)
+
+Laboratory.create!(
+  name: "General Computer Laboratory",
+  location: "Dolan Building, 3rd Floor",
+  department: computer_Eng
+)
+
+# Elect Laboratories
+Laboratory.create!(
+  name: "Workshop Laboratory",
+  location: "Dolan Building, 3rd Floor",
+  department: elec_Eng
+)
+
+Laboratory.create!(
+  name: "Electronics Laboratory",
+  location: "Dolan Building, 3rd Floor",
+  department: elec_Eng
+)
+
+puts "Database seeded successfully!"
+puts "Departments: #{Department.count}"
+puts "Students: #{Student.count}"
+puts "Teachers: #{Teacher.count}"
+puts "Laboratories: #{Laboratory.count}"
