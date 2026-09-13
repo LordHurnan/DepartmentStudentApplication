dd7dbcc975db6546188a360f1ef1da4fa0d15bc8
Vhien <vgranadil@gbox.adnu.edu.ph>
2026-09-13 12:00:06 +0800

    Implement teacher salary calculation,  add missing columns and improve layout using bootstrap

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -5,9 +5,15 @@ class Teacher < ApplicationRecord
   after_create :increment_department_count
   after_destroy :decrement_department_count
   after_update :department_was_changed
+  after_update :update_salary_if_rate_changed
 
   def display_name
-    [ department&.name, name ].compact_blank.join(" - ")
+    [department&.name, name].compact_blank.join(" - ")
+  end
+
+  def update_monthly_salary
+    self.monthly_salary = subjects.count * per_unit_rate.to_d
+    save!
   end
 
   private
@@ -26,5 +32,11 @@ class Teacher < ApplicationRecord
     old_department = Department.find_by(id: department_id_before_last_save)
     old_department&.decrement!(:teachers_count)
     department&.increment!(:teachers_count)
+  end
+
+  def update_salary_if_rate_changed
+    return unless saved_change_to_per_unit_rate?
+
+    update_monthly_salary
   end
 end
