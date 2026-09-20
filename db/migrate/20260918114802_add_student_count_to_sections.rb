47f85a6a052d93818097765ffecad1c435255932
Vhien <vgranadil@gbox.adnu.edu.ph>
2026-09-20 18:45:45 +0800

    Update Laboratories and Class Lists UI

--- db/migrate/20260918114802_add_student_count_to_sections.rb
+++ db/migrate/20260918114802_add_student_count_to_sections.rb
@@ -1,5 +1,5 @@
 class AddStudentCountToSections < ActiveRecord::Migration[8.1]
   def change
-    add_column :sections, :student_count, :integer, default: 0
+    add_column :sections, :student_count, :integer, default: 0 unless column_exists?(:sections, :student_count)
   end
 end
